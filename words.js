9f60318edbccb05102d196b02a8b95343514945f
Michael Freedman <mikfreedman@users.noreply.github.com>
2023-12-14 13:56:37 -0500

    Update words.js

--- words.js
+++ words.js
@@ -1,5 +1,8 @@
 const words = `
 
+Converse
+Salad tree is completely harvested
+Salad tree is completely ripe for harvesting
 6-10 minutes late
 1-5 minutes late 
 10+ minutes late
@@ -9,12 +12,17 @@ Dog spotted in the background
 Small talk about salad tree
 Changes mind mid-meeting and argues the opposite position that he’d just been arguing for
 “My hypothesis is that…”
-No show and didn’t schedule the meeting
-No show and scheduled the meeting
 Has a terrible cough
 Wearing a brewery hat
 Wearing a CO sports team hat
 Quotes self about "never mistake for stupidity that which can be explained by incentive structure"
 Only top half of face/hat visible
+Signal
+Synergy
+On time
+Comment in the chat
+Reaction in the chat
+Time is a flat circle
+States I'm just thinking out loud, proceeds to outline entire off-the-cuff solution
 
 `;
